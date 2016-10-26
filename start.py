e4cddda785b0d92830ee2216fbaa469af90c67a2
aseevlx <alexander@aseev.im>
2016-10-26 19:01:24 +0300

    Print all jira webhook data to terminal, working version

--- start.py
+++ start.py
@@ -1,30 +1,28 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request
+from flask import Flask, request, make_response, jsonify
 import json
 import traceback
 
 app = Flask(__name__)
 
-CERT_KEY = ('easr.ru.key')
-CERT = ('easr.ru.crt')
-context = (CERT, CERT_KEY)
-
 @app.route('/', methods=['POST'])
 def hook():
 	if request.method == 'POST':
 		print('Сейчас произошел POST-запрос')
-
 		data = request.data
+		data = data.decode('utf-8')
+
 		try:
 			jdata = json.loads(data)
 
-			if jdata['webhookEvent'] == 'jira:issue_created':
+			if jdata['webhookEvent'] == 'jira:issue_created' or jdata['webhookEvent'] == 'jira:issue_updated':
 				ticket_key = jdata['issue']['key']
 
-				print('Ticket: %s | %s' % (ticket_key, ticket_summary))
+				print('Ticket: %s' % (ticket_key))
+				print(jdata)
 				return make_response('ok', 200)
 			else:
-				msg = 'Invalid JIRA hook event. It should be "jira:issue_created"'
+				msg = 'Invalid JIRA hook event. It should be "jira:issue_created" or "_updated"'
 				print(msg)
 				return make_response(msg, 403)
 
@@ -36,9 +34,8 @@ def hook():
 
 @app.route('/', methods=['GET'])
 def index():
-	return make_response("Jira webhook")
+	return "Jira webhook"
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',
-		ssl_context=context,
+	app.run(host='127.0.0.1',
 		debug=True)
