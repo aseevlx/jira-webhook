4ae86185d48dd87f9efcf29674e638ec0534d0a5
aseevlx <alexander@aseev.im>
2016-10-27 19:49:45 +0300

    Now, script displays parameters from my Jira project (Holiday)

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, make_response
+from datetime import datetime, date
 import json
 from pprint import pprint
 import traceback
@@ -13,14 +14,29 @@ def hook():
 		data = request.data
 		data = data.decode('utf-8')
 
+		def string_to_date(datestring):
+			"""Convert string with date from Jira to Python date type"""
+			return datetime.strptime(datestring, '%Y-%m-%d')
+
+		def holiday_duration(start_of_holiday, end_of_holiday):
+			"""Calculate duration of holiday"""
+			return str(string_to_date(end_of_holiday) - string_to_date(start_of_holiday)).split()[0]
+
 		try:
 			jdata = json.loads(data)
 
 			if jdata['webhookEvent'] == 'jira:issue_created' or jdata['webhookEvent'] == 'jira:issue_updated':
-				ticket_key = jdata['issue']['key']
 
-				print('Ticket: %s' % (ticket_key))
-				pprint(jdata)
+				ticket_key = jdata['issue']['key']
+				reporter_name = jdata['issue']['fields']['reporter']['displayName']
+				issue_type = jdata['issue']['fields']['issuetype']['name']
+				start_of_holiday = jdata['issue']['fields']['customfield_16600']
+				end_of_holiday = jdata['issue']['fields']['customfield_16601']
+				holiday_duration = holiday_duration(start_of_holiday, end_of_holiday)
+
+				print('Ticket: %s | Reporter: %s' % (ticket_key, reporter_name))
+				print('Issue type is: %s' % issue_type)
+				print('Начало отпуска: %s, конец отпуска: %s, отпуск длится: %s' % (start_of_holiday, end_of_holiday,holiday_duration))
 				return make_response('ok', 200)
 			else:
 				msg = 'Invalid JIRA hook event. It should be "jira:issue_created" or "_updated"'
